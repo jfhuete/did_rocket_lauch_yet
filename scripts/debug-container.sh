ceb1d9fb8117ec2db161cee001400989eb8dd7d8
Juan Francisco Huete Verdejo <jf.huete@gmail.com>
2021-02-28 19:26:25 +0100

    Created script to launch a container to debug

---
+++ scripts/debug-container.sh
@@ -0,0 +1,8 @@
+docker run \
+  --rm \
+  -itd \
+  -v "$(pwd)":/app \
+  --name debug-app \
+  didrocketlaunchyet_app \
+  bash
+docker network connect --alias app didrocketlaunchyet_default debug-app
