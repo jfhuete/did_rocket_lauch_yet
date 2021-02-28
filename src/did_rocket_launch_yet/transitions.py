a5c74010f6803e6bf2e405a8b48ed160135c8e6b
Juan Francisco Huete Verdejo <jf.huete@gmail.com>
2021-02-28 19:32:43 +0000

    Created all transitions

--- src/did_rocket_launch_yet/transitions.py
+++ src/did_rocket_launch_yet/transitions.py
@@ -3,11 +3,54 @@
 from bernard.engine import Tr, triggers as trg
 from bernard.i18n import intents as its
 
-from did_rocket_launch_yet.states import Welcome, FirstFrame
+from did_rocket_launch_yet.states import (
+    Welcome, FirstFrame, RocketLaunched, RocketNotLaunched, LauchFound
+)
+
 
 transitions = [
     Tr(
         dest=Welcome,
         factory=trg.Text.builder(its.HELLO),
+    ),
+    Tr(
+        dest=FirstFrame,
+        origin=Welcome,
+        factory=trg.Text.builder(its.START),
+    ),
+    Tr(
+        dest=RocketNotLaunched,
+        origin=FirstFrame,
+        factory=trg.Text.builder(its.NO),
+    ),
+    Tr(
+        dest=RocketNotLaunched,
+        origin=RocketNotLaunched,
+        factory=trg.Text.builder(its.NO),
+    ),
+    Tr(
+        dest=RocketNotLaunched,
+        origin=RocketLaunched,
+        factory=trg.Text.builder(its.NO),
+    ),
+    Tr(
+        dest=RocketLaunched,
+        origin=FirstFrame,
+        factory=trg.Text.builder(its.YES),
+    ),
+    Tr(
+        dest=RocketLaunched,
+        origin=RocketNotLaunched,
+        factory=trg.Text.builder(its.YES),
+    ),
+    Tr(
+        dest=RocketLaunched,
+        origin=RocketLaunched,
+        factory=trg.Text.builder(its.YES),
+    ),
+    Tr(
+        dest=Welcome,
+        origin=LauchFound,
+        factory=trg.Text.builder(its.RESTART),
     ),
 ]
